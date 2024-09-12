d9342e7e38b375f80c5adfb0d54f30da17e8af52
Joxuwa <114867280+joxuwa@users.noreply.github.com>
2024-09-12 18:46:42 +0700

    Update cs2.sh

--- games/cs2.sh
+++ games/cs2.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+# Load modules
+source ./../get_colors.sh
+
+# Installing Dependencies
+echo -e "${YELLOW}Installing Dependencies${NC}"
+echo -e "========================================"
+
+# Create New User For Run The Server
+echo -e "${YELLOW}Create New User${NC}"
+echo -e "========================================"
+echo -e "User ${CYAN}cs2${NC} will be created"
+
+# Installing SteamCMD
+echo -e "${YELLOW}Installing SteamCMD${NC}"
+echo -e "========================================"
+
+# Installing Counter-Strike 2 Dedicated Server
+echo -e "${YELLOW}Installing Counter-Strike 2 Dedicated Server${NC}"
+echo -e "========================================"
+echo -e "[${MINT}START${NC}] Installing CS2 Dedicated Server from SteamCMD"
+echo -e "[${MINT}START${NC}] Fix missing data"
+
+# How To Use
+echo -e "${YELLOW}How To Use${NC}"
+echo -e "========================================"
+echo -e "[${CYAN}START SERVER${NC}] cd ~/serverfiles/game/bin/linuxsteamrt64 && screen -S 27015 ./cs2 -dedicated +map de_dust2"
+echo -e "[${CYAN}Exit Console${NC}] Ctrl+a d"
+echo -e "[${CYAN}Open Console${NC}] screen -r 27015"
